149243b8434f83b800d90cef84e23eee803d78ff
idiatou <idiatoudiallo027@gmail.com>
2026-04-18 20:38:53 -0400

    ajout de la methode de modification des donnees dans services

--- services/services_salle.py
+++ services/services_salle.py
@@ -4,7 +4,7 @@ class ServiceSalle:
     def __init__(self):
         self.dao_salle=DataSalle()
 
-#ajout de la methode ajouter salle , verification des donnees et la capacite qui doit etre > 1
+#ajout de la methode ajouter salle , verification des donnees et la capacite qui doit etre > 1---------------
     def ajouter_salle(self, salle):
         if not salle.code or not salle.description or not salle.categorie:
             return False, "toutes les informations sont obligatoires"
@@ -17,6 +17,16 @@ class ServiceSalle:
         except Exception as e:
             return False, f"Erreur base de données : {e}"
 
+# ajput de la methode modifier et la verification de la modification
+    def modifier_salle(self, salle):
+        if not salle.code or not salle.description or not salle.categorie or int(salle.capacite) < 1:
+            return False, "Données invalides pour la modification."
+
+        try:
+            self.dao_salle.update_salle(salle)
+            return True, "La salle a été modifiée "
+        except Exception as m:
+            return False, f"Erreur : {m}"
 
 
 
