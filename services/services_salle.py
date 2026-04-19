1b2ba51c5d3fd7e49e283344cd304e8dfea40864
idiatou <idiatoudiallo027@gmail.com>
2026-04-18 20:34:03 -0400

    ajout de la methode ajouter_salle dans services

--- services/services_salle.py
+++ services/services_salle.py
@@ -4,5 +4,19 @@ class ServiceSalle:
     def __init__(self):
         self.dao_salle=DataSalle()
 
+#ajout de la methode ajouter salle , verification des donnees et la capacite qui doit etre > 1
+    def ajouter_salle(self, salle):
+        if not salle.code or not salle.description or not salle.categorie:
+            return False, "toutes les informations sont obligatoires"
+
+        if int(salle.capacite) <1:
+            return False, "la capacite doit etre au moins 1"
+        try:
+            self.dao_salle.insert_salle(salle)
+            return True, "Succès : La salle a été ajoutée."
+        except Exception as e:
+            return False, f"Erreur base de données : {e}"
+
+
 
 
